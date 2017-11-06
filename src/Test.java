8b57a3d98598ce6abe150bc2b2886c676a03a877
Luca Pascucci <luca.pascucci@studio.unibo.it>
2017-11-06 12:41:03 +0100

    Aggiunti commenti

--- src/Test.java
+++ src/Test.java
@@ -1,25 +1,24 @@
 import org.antlr.v4.runtime.*;
 
 public class Test {
-    public static void main(String[] args) throws Exception {
-     
-        String fileName = "quicksort.fool.asm";
-                
-        CharStream chars = CharStreams.fromFileName(fileName);
-        SVMLexer lexer = new SVMLexer(chars);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        SVMParser parser = new SVMParser(tokens);
-        
-        
-        parser.assembly();
-        
-        System.out.println("You had: "+lexer.lexicalErrors+" lexical errors and "+parser.getNumberOfSyntaxErrors()+" syntax errors.");
-        if (lexer.lexicalErrors>0 || parser.getNumberOfSyntaxErrors()>0) System.exit(1);
+	public static void main(String[] args) throws Exception {
 
-        System.out.println("Starting Virtual Machine...");
-        ExecuteVM vm = new ExecuteVM(parser.code);
-        vm.cpu();
-        
-      
-    }
+		String fileName = "quicksort.fool.asm";
+
+		CharStream chars = CharStreams.fromFileName(fileName);
+		SVMLexer lexer = new SVMLexer(chars);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		SVMParser parser = new SVMParser(tokens);
+
+		parser.assembly();
+
+		System.out.println("You had: " + lexer.lexicalErrors + " lexical errors and " + parser.getNumberOfSyntaxErrors() + " syntax errors.");
+		if (lexer.lexicalErrors > 0 || parser.getNumberOfSyntaxErrors() > 0) {
+			System.exit(1);
+		}
+
+		System.out.println("Starting Virtual Machine...");
+		ExecuteVM vm = new ExecuteVM(parser.code);
+		vm.cpu();
+	}
 }
